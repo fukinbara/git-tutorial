0e33beffb406bb3fc1bc9a6d65f03798c43537bb
fukinbara <fukinbara.momoka@gmail.com>
2020-10-22 11:46:08 +0900

    first commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+for i in 1..100 do 
+    if i % 3 == 0 && i % 5 == 0
+      print "FizzBuzz\n"
+    elsif i % 3 == 0
+      print "Fizz\n"
+    elsif i % 5 == 0
+      print "Buzz\n"
+    else 
+      puts "{#a}\n"
+    end
+
+end
+
